0ca188400e38403e75cbb6f6492a32028eac6e72
Massimo Savino <north.twilight@gmail.com>
2022-08-05 16:32:04 -0700

    HWS16-MapKit added, added ViewModel to that

--- HackingWithSwift-UIKit/HWS16-CapitalCities/HWS16-CapitalCities/ViewController.swift
+++ HackingWithSwift-UIKit/HWS16-CapitalCities/HWS16-CapitalCities/ViewController.swift
@@ -9,35 +9,19 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController {
-    private struct Constants {
-        static let londonInfo = "Home to the 2012 Summer Olympics"
-        static let osloInfo = "Founded over a thousand years ago."
-        static let parisInfo = "Often called the City of Light"
-        static let romeInfo = "Has a whole country inside it."
-        static let washingtonInfo = "Named after George himself."
-    }
-    
+
     @IBOutlet weak var mapView: MKMapView!
-    
-    let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: Constants.londonInfo)
-    let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: Constants.osloInfo)
-    let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: Constants.parisInfo)
-    let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: Constants.romeInfo)
-    let washington = Capital(title: "Washington", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: Constants.washingtonInfo)
-    
-    lazy var capitals: [Capital] = [london, oslo, paris, rome, washington]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupCapitals()
+        setupViewModel()
     }
     
-    func setupCapitals() {
-        for capital in capitals {
+    func setupViewModel() {
+        let viewModel = ViewModel()
+        for capital in viewModel.cities {
             mapView.addAnnotation(capital)
         }
     }
-
-
 }
 
---
+++ HackingWithSwift-UIKit/HWS16-CapitalCities/HWS16-CapitalCities/ViewModel.swift
@@ -0,0 +1,78 @@
+//
+//  ViewModel.swift
+//  HWS16-CapitalCities
+//
+//  Created by Massimo Savino on 2022-08-05.
+//
+
+import Foundation
+import MapKit
+import UIKit
+
+protocol ViewModelType {
+    var cities: [Capital] { get }
+}
+class ViewModel: ViewModelType {
+    private struct Constants {
+        static let londonInfo = "Home to the 2012 Summer Olympics"
+        static let osloInfo = "Founded over a thousand years ago."
+        static let parisInfo = "Often called the City of Light"
+        static let romeInfo = "Has a whole country inside it."
+        static let washingtonInfo = "Named after George himself."
+        
+        static let londonTitle = "London"
+        static let osloTitle = "Oslo"
+        static let parisTitle = "Paris"
+        static let romeTitle = "Rome"
+        static let washingtonTitle = "Washington"
+        
+        static let londonLatitude = 51.507222
+        static let osloLatitude = 59.95
+        static let parisLatitude = 48.8567
+        static let romeLatitude = 41.9
+        static let washingtonLatitude = 38.895111
+        
+        static let londonLongitude = -0.1275
+        static let osloLongitude = 10.75
+        static let parisLongitude = 2.3508
+        static let romeLongitude = 12.5
+        static let washingtonLongitude = -77.036667
+    }
+    
+    let london = Capital(
+        title: Constants.londonTitle,
+        coordinate: CLLocationCoordinate2D(
+            latitude: Constants.londonLatitude,
+            longitude: Constants.londonLongitude),
+        info: Constants.londonInfo)
+    
+    let oslo = Capital(
+        title: Constants.osloTitle,
+        coordinate: CLLocationCoordinate2D(
+            latitude: Constants.osloLatitude,
+            longitude: Constants.osloLongitude),
+        info: Constants.osloInfo)
+    
+    let paris = Capital(
+        title: Constants.parisTitle,
+        coordinate: CLLocationCoordinate2D(
+            latitude: Constants.parisLatitude,
+            longitude: Constants.parisLongitude),
+        info: Constants.parisInfo)
+    
+    let rome = Capital(
+        title: Constants.romeTitle,
+        coordinate: CLLocationCoordinate2D(
+            latitude: Constants.romeLatitude,
+            longitude: Constants.romeLongitude),
+        info: Constants.romeInfo)
+    
+    let washington = Capital(
+        title: Constants.washingtonTitle,
+        coordinate: CLLocationCoordinate2D(
+            latitude: Constants.washingtonLatitude,
+            longitude: Constants.washingtonLongitude),
+        info: Constants.washingtonInfo)
+    
+    lazy var cities: [Capital] = [london, oslo, paris, rome, washington]
+}
