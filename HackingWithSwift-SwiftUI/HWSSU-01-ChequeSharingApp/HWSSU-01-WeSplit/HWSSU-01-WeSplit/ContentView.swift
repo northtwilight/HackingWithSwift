d26b5ea3c8ceafb32712bcf053c9929feef11df4
Massimo Savino <north.twilight@gmail.com>
2022-08-24 10:49:50 -0700

    Rough draft with a lot of excess content. Will make side project with blank contents next

--- HackingWithSwift-SwiftUI/HWSSU-01-ChequeSharingApp/HWSSU-01-WeSplit/HWSSU-01-WeSplit/ContentView.swift
+++ HackingWithSwift-SwiftUI/HWSSU-01-ChequeSharingApp/HWSSU-01-WeSplit/HWSSU-01-WeSplit/ContentView.swift
@@ -10,22 +10,57 @@ import SwiftUI
 struct ContentView: View {
     private struct Constants {
         static let reset = "Press this button to reset Tap Count to 0"
+        static let enterName = "Enter your name"
+        static let helloWorld = "Hello, world"
+        static let swiftUItext = "SwiftUI"
+        static let harry = "Harry"
+        static let hermione = "Hermione"
+        static let ron = "Ron"
+        static let selectYourStudent = "Select your student"
+        
         static func provide(_ tapCount: Int) -> String {
             "Tap Count: \(tapCount)"
         }
+        
+        static func yourNameIs(_ name: String) -> String {
+            "Your name is: \(name)"
+        }
     }
     @State private var tapCount = 0
+    @State private var name = ""
+    @State private var selectedStudent = Constants.harry
+    
+    let students = [
+        Constants.harry,
+        Constants.hermione,
+        Constants.ron
+    ]
     
     var body: some View {
-        Button(Constants.provide(tapCount)) {
-            self.tapCount += 1
+        NavigationView {
+            Form {
+                Picker(Constants.selectYourStudent, selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField(Constants.enterName, text: $name)
+                
+                Text(Constants.yourNameIs(name))
+                
+                Button(Constants.provide(tapCount)) {
+                    self.tapCount += 1
+                }
+                .padding()
+                
+                Button(Constants.reset) {
+                    self.tapCount = 0
+                }
+                .padding()
+                
+            }.navigationTitle(Constants.swiftUItext)
         }
-        .padding()
-        
-        Button(Constants.reset) {
-            self.tapCount = 0
-        }
-        .padding()
     }
 }
 
