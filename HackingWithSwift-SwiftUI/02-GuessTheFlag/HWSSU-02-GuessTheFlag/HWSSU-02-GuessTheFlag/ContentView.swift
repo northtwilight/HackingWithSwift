25727285f428c4a034d45576ee12426b91b2364e
Massimo Savino <north.twilight@gmail.com>
2022-08-24 16:17:31 -0700

    HWSSU-02 Guess the Flag started. Added Git LFS tracking for PNGs

---
+++ HackingWithSwift-SwiftUI/02-GuessTheFlag/HWSSU-02-GuessTheFlag/HWSSU-02-GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HWSSU-02-GuessTheFlag
+//
+//  Created by Massimo Savino on 2022-08-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HackingWithSwift-SwiftUI/02-GuessTheFlag/HWSSU-02-GuessTheFlag/HWSSU-02-GuessTheFlag/HWSSU_02_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSSU_02_GuessTheFlagApp.swift
+//  HWSSU-02-GuessTheFlag
+//
+//  Created by Massimo Savino on 2022-08-24.
+//
+
+import SwiftUI
+
+@main
+struct HWSSU_02_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
