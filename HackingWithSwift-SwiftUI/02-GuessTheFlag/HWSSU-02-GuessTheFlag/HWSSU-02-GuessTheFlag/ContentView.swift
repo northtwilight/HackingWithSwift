98c18d7b3314a2f9e05b32e6ffca79d061253eed
Massimo Savino <north.twilight@gmail.com>
2022-08-24 18:37:56 -0700

    HWSSU-02 Tried out different gradients

--- HackingWithSwift-SwiftUI/02-GuessTheFlag/HWSSU-02-GuessTheFlag/HWSSU-02-GuessTheFlag/ContentView.swift
+++ HackingWithSwift-SwiftUI/02-GuessTheFlag/HWSSU-02-GuessTheFlag/HWSSU-02-GuessTheFlag/ContentView.swift
@@ -11,8 +11,33 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             VStack(spacing: 0) {
-                Color.red
-                Color.blue
+//                LinearGradient(
+//                    gradient: Gradient(
+//                        stops: [
+//                            Gradient.Stop(color: .white, location: 0.45),
+//
+//                            Gradient.Stop(color: .black, location: 0.55),
+//                                ]),
+//                    startPoint: .top,
+//                    endPoint: .bottom)
+                
+//                RadialGradient(
+//                    gradient: Gradient(colors: [.blue, .black]),
+//                    center: .center,
+//                    startRadius: 20,
+//                    endRadius: 200)
+                
+                AngularGradient(
+                    gradient: Gradient(
+                        colors: [
+                            .red,
+                            .yellow,
+                            .green,
+                            .blue,
+                            .purple,
+                            .red
+                        ]),
+                    center: .center)
             }
             Text("Your content")
                 .foregroundColor(.secondary)
