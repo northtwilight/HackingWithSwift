7adf9bd6a68c5d98a7a2b72aea380da62f697659
Massimo Savino <north.twilight@gmail.com>
2022-08-24 17:59:34 -0700

    HWSSU-02 trying out translucent shades

--- HackingWithSwift-SwiftUI/02-GuessTheFlag/HWSSU-02-GuessTheFlag/HWSSU-02-GuessTheFlag/ContentView.swift
+++ HackingWithSwift-SwiftUI/02-GuessTheFlag/HWSSU-02-GuessTheFlag/HWSSU-02-GuessTheFlag/ContentView.swift
@@ -9,8 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
+            }
+            Text("Your content")
+                .foregroundColor(.secondary)
+                .padding(50)
+                .background(.ultraThinMaterial)
+        }
+        /**
+         
+        VStack {
+            HStack {
+                Text("1").padding()
+                Text("2").padding()
+                Text("3").padding()
+            }
+            HStack {
+                Text("4").padding()
+                Text("5").padding()
+                Text("6").padding()
+            }
+            HStack {
+                Text("7").padding()
+                Text("8").padding()
+                Text("9").padding()
+            }
+        }
+        
+        */
     }
 }
 
